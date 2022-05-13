b6f98ed8af9378a7ea2bdd015caf22b923a094c0
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2022-05-13 19:53:45 +0530

    update

---
+++ dist/components/Avatar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ActivityIndicator, Button, View, StyleSheet } from 'react-native';
+
+interface AvatarProps {
+  loading?: boolean;
+}
+
+const Avatar = (props: AvatarProps) => {
+  const {
+    loading,
+  } = props;
+
+  return (
+    <View style={styles.wrapperStyle}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapperStyle:{
+    backgroundColor:"red",
+    
+  }
+})
+
+export default Avatar;
